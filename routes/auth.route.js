32bbe3b8122837ad1e2177dbef6f3fb419ceef60
ryu820 <rhju820@naver.com>
2023-04-06 05:14:50 +0900

    [feat]로그인 기본세팅

---
+++ routes/auth.route.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const { Users, Teams, Companys } = require("../models");
+const CustomError = require("../middlewares/errorhandler.js")
+const env = process.env
+const router = express.Router();
+
+//대표자 로그인
+//localhost:3003/adminlogin
+router.post("/adminlogin", async (req, res, next) => {
+    try {
+        const { companyId, password } = req.body;
+        const user = Users.findOne({ where: { companyId } })
+        const team = Teams.findOne({ where: { teamId: user.teamId } })
+        if (user.userId !== companyId || user.password !== password) {
+            throw new CustomError("아이디 혹은 비밀번호를 확인해주세요.", 401)
+        }
+        const token = jwt.sign({
+            userId: user.userId,
+            teamName: team.teamName,
+            authLevel: user.authLevel
+        }, env.SECRET_KEY);
+
+        res.status(200).json({ "message": "회원가입에 성공했습니다", "token": `Bearer ${token}` })
+    } catch (err) {
+        next(err)
+    }
+})
+
+//일반 유저 로그인
+//localhost:3003/login
+router.post("/login", async (req, res, next) => {
+    try {
+        const { companyId, userId, password } = req.body;
+        const user = Users.findOne({ where: { userId } })
+        const team = Teams.findOne({ where: { teamId: user.teamId } })
+        const company = Companys.findOne({ where: { companyId: companyId } })
+
+        if (user.userId !== userId || user.password !== password) {
+            throw new CustomError("아이디 혹은 비밀번호를 확인해주세요.", 401)
+        } else if (!company) {
+            throw new CustomError("회사코드를 확인해주세요.", 401)
+        }
+
+        const token = jwt.sign({
+            userId: user.userId,
+            teamName: team.teamName,
+            authLevel: user.authLevel
+        }, env.SECRET_KEY);
+
+        res.status(200).json({ "message": "회원가입에 성공했습니다", "token": `Bearer ${token}` })
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+//초기 비밀번호 변경
+//localhost:3003/login/rePassword
+router.post("/login/rePassword", async (req, res, next) => {
+    const { password } = req.body;
+    try{
+        await Users.update({ password },{where: { userId }})
+        res.status(200).json({"message" : "비밀번호 변경에 성공했습니다"})
+    }catch(err){
+        next(err)
+    }
+})
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const AuthRouter = require("../routes/auth.route.js")
+
+router.use("/",AuthRouter)
 
 module.exports = router;
