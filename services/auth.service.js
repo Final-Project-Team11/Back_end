ce82896a4f1b437a34392f863a2f9f0f8ad19453
ryu820 <rhju820@naver.com>
2023-04-07 22:51:24 +0900

    [fix]유저로그인 수정 업데이트 #2

--- services/auth.service.js
+++ services/auth.service.js
@@ -19,6 +19,17 @@ class AuthService {
         }
         return user;
     };
+    checkUserIdPassword = async ({ userId, password }) => {
+        const user = await this.AuthRepository.findByUserId({ userId });
+        const checkpassword = await bcrypt.compare(password, user.password);
+
+        if (user.userId !== userId) {
+            throw new CustomError("아이디 혹은 비밀번호를 확인해주세요.", 401);
+        } else if (!checkpassword) {
+            throw new CustomError("아이디 혹은 비밀번호를 확인해주세요.", 401);
+        }
+        return user;
+    };
     adminLogin = async ({ user }) => {
         const team = await this.AuthRepository.findTeamById({ user });
         const token = jwt.sign(
