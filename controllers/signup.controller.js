fd19bd133b889789809a3348361a28ed8c2eab5a
ryu820 <rhju820@naver.com>
2023-04-07 15:45:27 +0900

    [fix]에러메세지 수정 #1

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -19,44 +19,46 @@ class SignupController {
         } = req.body;
 
         //Joi
-        const regex = /^[a-zA-Z0-9]{4,}$/;
+        const regex = /^[a-zA-Z0-9]$/;
         const schema = Joi.object({
             companyName: Joi.string().required().messages({
                 "string.base": "companyName 필드는 문자열로 이루어져야 합니다.",
                 "string.empty": "회사명을 입력해 주세요.",
-                "any.required": "이 필드는 필수입니다.",
+                "any.required": "이 필드는 비어 있을 수 없습니다.",
             }),
             address: Joi.string().required().messages({
                 "string.base": "address 필드는 문자열로 이루어져야 합니다.",
                 "string.empty": "회사 주소를 입력해 주세요.",
-                "any.required": "이 필드는 필수입니다.",
+                "any.required": "이 필드는 비어 있을 수 없습니다.",
             }),
             ceoName: Joi.string().required().messages({
                 "string.base": "ceoName 필드는 문자열로 이루어져야 합니다.",
                 "string.empty": "대표자 이름을 입력해 주세요.",
-                "any.required": "이 필드는 필수입니다.",
+                "any.required": "이 필드는 비어 있을 수 없습니다.",
             }),
             companyNum: Joi.number().required().messages({
                 "number.base": "companyNum 필드는 숫자로 이루어져야 합니다.",
                 "string.empty": "사업자 등록 번호를 입력해 주세요.",
-                "any.required": "이 필드는 필수입니다.",
+                "any.required": "이 필드는 비어 있을 수 없습니다.",
             }),
             ceoNum: Joi.number().required().messages({
                 "number.base": "ceoNum 필드는 숫자로 이루어져야 합니다.",
                 "string.empty": "대표자 연락처를 입력해 주세요.",
-                "any.required": "이 필드는 필수입니다.",
+                "any.required": "이 필드는 비어 있을 수 없습니다.",
             }),
-            companyId: Joi.string().pattern(regex).required().messages({
+            companyId: Joi.string().min(5).pattern(regex).required().messages({
                 "string.base": "companyId 필드는 문자열로 이루어져야 합니다.",
-                "string.pattern.base": "아이디의 형식에 맞지 않습니다.",
+                "string.pattern.base": "아이디는 영문 대/소문자와 숫자만 포함 가능합니다.",
+                "string.min": "아이디는 최소 5글자여야 합니다.",
                 "string.empty": "대표자 Id를 입력해 주세요.",
-                "any.required": "이 필드는 필수입니다.",
+                "any.required": "이 필드는 비어 있을 수 없습니다.",
             }),
-            password: Joi.string().pattern(regex).required().messages({
+            password: Joi.string().min(5).pattern(regex).required().messages({
                 "string.base": "password 필드는 문자열로 이루어져야 합니다.",
-                "string.pattern.base": "패스워드의 형식에 맞지 않습니다.",
+                "string.pattern.base": "비밀번호는 영문 대/소문자와 숫자만 포함 가능합니다.",
+                "string.min": "비밀번호는 최소 5글자여야 합니다.",
                 "string.empty": "비밀번호를 입력해 주세요.",
-                "any.required": "이 필드는 필수입니다.",
+                "any.required": "이 필드는 비어 있을 수 없습니다.",
             }),
         });
 
@@ -72,7 +74,7 @@ class SignupController {
             });
     
             if (validate.error) {
-                throw new CustomError(validate.error.message, 400);
+                throw new CustomError(validate.error.message, 401);
             } else {
                 console.log("Valid input!");
             }
