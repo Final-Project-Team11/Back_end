0b6d70de21c022a07291ef7e3b72f69fce035b8c
ryu820 <rhju820@naver.com>
2023-04-08 16:58:44 +0900

    [fix]투두리스트 기본 세팅 완료 #18

--- routes/myPage.route.js
+++ routes/myPage.route.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const { CategoryTodos, Todos } = require("../models");
-const CustomError = require("../middlewares/errorHandler")
+const CustomError = require("../middlewares/errorHandler");
 const authmiddleware = require("../middlewares/auth-middleware.js");
 const router = express.Router();
+
+//투두리스트 전체 조회
+//localhost:3003/feed
+router.get("/feed", authmiddleware, async (req, res, next) => {
+    const { userId } = res.locals.user;
+    const existCategory = await CategoryTodos.findAll({ where: { userId } });
+    
+    //map?
+    const list = await Promise.all(existCategory.map(async(todo) => {
+        const categoryId = todo.categoryId
+        const existTodos = await Todos.findAll({ where: { userId,categoryId } });
+        return {
+            "categoryId" : todo.categoryId,
+            "categoryName" : todo.categoryName,
+            todos : existTodos
+        }
+    }))
+    
+    res.status(200).json({feed : list})
+});
 
 //카테고리 생성
 //localhost:3003/feed/category
@@ -96,7 +116,8 @@ router.delete("/feed/todo/:todoId", authmiddleware, async (req, res, next) => {
             throw new CustomError("존재하지 않는 투두리스트입니다.", 401);
         } else if (existTodos.userId !== userId) {
             throw new CustomError(
-                "해당 투두리스트에 대한 권한이 없습니다.",401
+                "해당 투두리스트에 대한 권한이 없습니다.",
+                401
             );
         }
         await Todos.destroy({ where: { todoId, userId } });
@@ -108,14 +129,38 @@ router.delete("/feed/todo/:todoId", authmiddleware, async (req, res, next) => {
 
 //투두리스트 체크
 //localhost:3003/feed/todo/{todoId}
-router.patch("/feed/todo/:todoId",authmiddleware, async (req, res, next) => {
-    const {todoId} = req.params;
-    const {userId} = res.locals.user;
+router.patch("/feed/todo/:todoId", authmiddleware, async (req, res, next) => {
+    const { todoId } = req.params;
+    const { userId } = res.locals.user;
+    const existTodos = await Todos.findOne({ where: { todoId } });
 
-    
-    
-})
+    if (!existTodos) {
+        throw new CustomError("존재하지 않는 투두리스트입니다.", 401);
+    } else if (existTodos.userId !== userId) {
+        throw new CustomError("해당 투두리스트에 대한 권한이 없습니다.", 401);
+    }
 
-
+    if (existTodos.isDone === false) {
+        await Todos.update(
+            {
+                isDone: true,
+            },
+            {
+                where: { userId, todoId },
+            }
+        );
+        res.status(200).json({ message: "투두 리스트가 체크되었습니다." });
+    } else if (existTodos.isDone === true) {
+        await Todos.update(
+            {
+                isDone: false,
+            },
+            {
+                where: { userId, todoId },
+            }
+        );
+        res.status(200).json({ message: "투두 리스트가 체크 해제되었습니다." });
+    }
+});
 
 module.exports = router;
