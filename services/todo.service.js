785fb440caa5990e05d1a1618c24f4a81ecd76d6
ryu820 <rhju820@naver.com>
2023-04-11 15:08:04 +0900

    [fix]투두리스트 파일 분리 완료 #47

---
+++ services/todo.service.js
@@ -0,0 +1,85 @@
+const CustomError = require("../middlewares/errorHandler");
+const TodoRepository = require("../repositories/todo.repository.js")
+class TodoService {
+    constructor() {
+        this.TodoRepository = new TodoRepository();
+    }
+    getTodos = async ({ userId }) => {
+        const existCategory = await this.TodoRepository.findAllCategory({
+            userId,
+        });
+
+        const list = await Promise.all(
+            existCategory.map(async (todo) => {
+                const categoryId = todo.categoryId;
+                const existTodos = await this.TodoRepository.findAlltodos({
+                    userId,
+                    categoryId,
+                });
+                return {
+                    categoryId: todo.categoryId,
+                    categoryName: todo.categoryName,
+                    todos: existTodos,
+                };
+            })
+        );
+        return list;
+    };
+
+    createCategory = async ({ userId, category }) => {
+        await this.TodoRepository.createCategory({ userId, category });
+    };
+    checkCategory = async ({ categoryId, userId }) => {
+        const existCategory = await this.TodoRepository.findOneCategory({
+            categoryId,
+        });
+        if (!existCategory) {
+            throw new CustomError("존재하지 않는 카테고리입니다.", 401);
+        } else if (existCategory.userId !== userId) {
+            throw new CustomError("해당 카테고리에 대한 권한이 없습니다.", 401);
+        }
+    };
+    checkTodo = async ({ todoId, userId }) => {
+        const existTodos = await this.TodoRepository.findOneTodo({ todoId });
+        if (!existTodos) {
+            throw new CustomError("존재하지 않는 투두리스트입니다.", 401);
+        } else if (existTodos.userId !== userId) {
+            throw new CustomError(
+                "해당 투두리스트에 대한 권한이 없습니다.",
+                401
+            );
+        }
+        return existTodos;
+    };
+    createTodo = async ({ userId, categoryId, content, isDone }) => {
+        await this.TodoRepository.createTodo({
+            userId,
+            categoryId,
+            content,
+            isDone,
+        });
+    };
+    deleteCategory = async ({ categoryId, userId }) => {
+        await this.TodoRepository.deleteCategory({ categoryId, userId });
+    };
+    deleteTodo = async ({ todoId, userId }) => {
+        await this.TodoRepository.deleteTodo({ todoId, userId });
+    };
+    completeTodo = async ({ userId, todoId, existTodos }) => {
+        let message;
+        let isDone;
+        if (existTodos.isDone === false) {
+            isDone = true;
+            await this.TodoRepository.updateTodo({ userId, todoId, isDone });
+            message = "투두 리스트가 체크되었습니다.";
+            return message;
+        } else if (existTodos.isDone === true) {
+            isDone = false;
+            await this.TodoRepository.updateTodo({ userId, todoId, isDone });
+            message = "투두 리스트가 체크 해제되었습니다.";
+            return message;
+        }
+    };
+}
+
+module.exports = TodoService;
