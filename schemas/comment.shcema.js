d118fc8de12eea48a30d4ec767cd167f930cf8c4
wndudtjd <dudtjdwn1575@naver.com>
2023-04-15 22:12:21 +0900

    [FIX] 수정사항 업데이트 #99

--- schemas/comment.shcema.js
+++ schemas/comment.shcema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi')
 
-const schema = Joi.object({
+const commentSchema = Joi.object({
     comment: Joi.string().required().messages({
         "string.base": "comment 필드는 날짜로 이루어져야 합니다.",
         "string.empty": "댓글을 입력해 주세요.",
