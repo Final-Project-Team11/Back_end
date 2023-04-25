b85e6419fc0353c910f02f5a127e2009a7d4ec9c
ryu820 <rhju820@naver.com>
2023-04-25 23:45:18 +0900

    [FIX]로그인, 회원가입 RESTful 수정 #214

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const scheduleManageRouter = require("./scheduleManage.route");
 const vacationManageRouter = require("./vacationManage.route");
 const userManageRouter = require("./userManage.route");
-const AuthRouter = require("../routes/auth.route");
-const UserRouter = require("../routes/signup.route");
+const authRouter = require("../routes/auth.route");
+const userRouter = require("../routes/signup.route");
 const submitRouter = require("./submit.route");
 const myPageRputer = require("./myPage.route.js")
 const commentRouter = require('./comment.route')
 const mainPageRouter = require('./mainPage.route')
 const todoRouter = require("../routes/todo.route");
 
-router.use("/", [UserRouter,AuthRouter,submitRouter,myPageRputer,commentRouter,mainPageRouter]);
+router.use("/", [authRouter,submitRouter,myPageRputer,commentRouter,mainPageRouter]);
+router.use("/members", userRouter);
 router.use("/users", userManageRouter);
 router.use("/schedule", scheduleManageRouter);
 router.use("/vacation", vacationManageRouter);
