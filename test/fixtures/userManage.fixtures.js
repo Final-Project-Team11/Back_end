fab9aca9548ef111a93fee85bdb6c2077e92276f
suhyeon <minsoohyeon1234@gmail.com>
2023-04-27 20:05:06 +0900

    [TEST]관리자 미들웨어 테스트 코드 작성 #263

---
+++ test/fixtures/userManage.fixtures.js
@@ -0,0 +1,14 @@
+exports.managerUser = {
+    userId: 'myelin23',
+    authLevel: 1,
+    teamName: 'Team',
+    companyId: 1,
+};
+
+
+exports.user = {
+    userId: 'myelin23',
+    authLevel: 3,
+    teamName: 'Team',
+    companyId: 1,
+};
