92d5cc71637f4bae02563ff55fda00929957d2f5
wndudtjd <dudtjdwn1575@naver.com>
2023-04-28 00:47:09 +0900

    [TEST] 휴가 조회 사용자 권한이 존재하지 않을떄. #115

--- test/unit/services/mainPage.service.test.js
+++ test/unit/services/mainPage.service.test.js
@@ -4,11 +4,9 @@ const {
     mockData,
     vacationData,
     scheduleData,
-    reportData,
-    otherData,
-    meetingReportData,
     issueData,
     meetingData,
+    eventData,
 } = require('../../fixtures/mainPage.fixtures')
 
 const mockMainPageRepository = () => ({
@@ -20,9 +18,16 @@ const mockMainPageRepository = () => ({
     findTotalMeeting: jest.fn(),
     findTotalMeetingReport: jest.fn(),
     findTeamName: jest.fn(),
+    findTotalEvent: jest.fn(),
+    findCompanyId: jest.fn()
 });
 
 const teamName = '개발팀'
+
+const expectedError = {
+    status: 401,
+    message: '권한이 존재하지않습니다.'
+};
 
 describe('MainPageService Test', () => {
     let mainPageService = new MainPageService()
@@ -34,7 +39,7 @@ describe('MainPageService Test', () => {
     })
 
     test('findTotalVacation', async() => {
-
+        mainPageService.mainPageRepository.findCompanyId.mockResolvedValue({companyId: 2})
         // mockMainPageRepository에서 findTeamName 메소드가 호출될 때 반환될 가짜 데이터를 설정한다.
         mainPageService.mainPageRepository.findTeamName.mockResolvedValue({teamName});
 
@@ -49,26 +54,33 @@ describe('MainPageService Test', () => {
         })
     })
 
+    test('findTotalVacation 사용자 권한이 존재하지 않을 때', async() => {
+        mainPageService.mainPageRepository.findCompanyId.mockResolvedValue({companyId: 4})
+
+        // rejects.toThrow() 메소드를 사용하여 해당 메소드가 에러를 throw하는지 검증합니다.
+        expect(mainPageService.findTotalVacation(mockData)).rejects.toThrow(CustomError);
+        expect(mainPageService.findTotalVacation(mockData)).rejects.toMatchObject(expectedError);
+
+        expect(mainPageService.mainPageRepository.findCompanyId).toHaveBeenCalledWith(mockData.teamId);
+    })
+
     test('findTotalSchedule', async() => {
+        mainPageService.mainPageRepository.findCompanyId.mockResolvedValue({companyId: 2})
         // mockMainPageRepository에서 findTeamName 메소드가 호출될 때 반환될 가짜 데이터를 설정한다.
         mainPageService.mainPageRepository.findTeamName.mockResolvedValue({teamName});
         mainPageService.mainPageRepository.findTotalSchedule.mockResolvedValue(scheduleData);
-        mainPageService.mainPageRepository.findTotalReport.mockResolvedValue(reportData);
-        mainPageService.mainPageRepository.findTotalOther.mockResolvedValue(otherData);
-        mainPageService.mainPageRepository.findTotalMeetingReport.mockResolvedValue(meetingReportData);
         mainPageService.mainPageRepository.findTotalIssue.mockResolvedValue(issueData);
         mainPageService.mainPageRepository.findTotalMeeting.mockResolvedValue(meetingData);
+        mainPageService.mainPageRepository.findTotalEvent.mockResolvedValue(eventData);
 
         const result = await mainPageService.findTotalSchedule(mockData)
 
         expect(result).toEqual({
             teamName,
             schedule: scheduleData,
-            report: reportData,
-            other: otherData,
             issue: issueData,
             meeting: meetingData,
-            meetingReport: meetingReportData,
+            event: eventData
         })
     })
 })
