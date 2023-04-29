405c4c7aae7501ccc9b29b3a4517cb479c84cc04
ryu820 <rhju820@naver.com>
2023-04-29 10:26:04 +0900

    [FIX]signup service test 완료 #275

--- test/fixtures/signup.fixtures.js
+++ test/fixtures/signup.fixtures.js
@@ -1,32 +1,32 @@
 exports.SignupinvalidData = {
-    companyName : "testcompany",
-    address : "서울시 어쩌고",
-    ceoName : "testman",
-    companyNum :  "1234567899",
-    ceoNum : "01012345678",
-    companyId : "test1",
-    password : "1234aaaa",
+    companyName: "testcompany",
+    address: "서울시 어쩌고",
+    ceoName: "testman",
+    companyNum: "1234567899",
+    ceoNum: "01012345678",
+    companyId: "test1",
+    password: "1234aaaa",
 };
 exports.SignupvalidData = {
-    companyName : "testcompany",
-    address : "서울시 어쩌고",
-    ceoName : "testman",
-    companyNum :  "1234567899",
-    ceoNum : "01012345678",
-    companyId : "test1",
-    password : "1234aaaa!!",
+    companyName: "testcompany",
+    address: "서울시 어쩌고",
+    ceoName: "testman",
+    companyNum: "1234567899",
+    ceoNum: "01012345678",
+    companyId: "test1",
+    password: "1234aaaa!!",
 };
 exports.CheckIDinvalidData = {
-    companyId : "test1!!",
+    companyId: "test1!!",
 };
 exports.CheckIDvalidData = {
-    companyId : "test1",
+    companyId: "test1",
 };
 exports.SignupInsertSchema = {
     companyName: "testcompany",
     address: "서울시 어쩌고",
     ceoName: "testman",
-    companyNum:  "1234567899",
+    companyNum: "1234567899",
     ceoNum: "01012345678",
     teamName: "CEO",
     companyId: "test1",
@@ -37,3 +37,24 @@ exports.SignupInsertSchema = {
     authLevel: 1,
     job: "CEO",
 }
+
+exports.CheckCompanyInsertSchema = {
+    companyName: "testcompany"
+}
+
+exports.CheckCompanyIdInsertSchema = {
+    companyId: "test1"
+}
+
+exports.checkCompanyErrorResultSchema = [
+    {
+        companyId: "test1",
+        companyName: "testcompany",
+        ceoNum: "01012345678",
+        address: "서울시 어쩌고",
+        ceoName: "testman",
+        ceoNum: "01012345678",
+        createdAt: "2023-04-28T06:34:45.000Z",
+        updatedAt: "2023-04-28T06:34:45.000Z"
+    }
+]
