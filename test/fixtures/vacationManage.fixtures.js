7d6093752c3a8ff3f7c6c890f1b53ffb7c70256b
suhyeon <minsoohyeon1234@gmail.com>
2023-05-02 20:40:56 +0900

    [TEST]vacationManage 컨트롤러 유닛테스트 작성 완료 #314

---
+++ test/fixtures/vacationManage.fixtures.js
@@ -0,0 +1,35 @@
+exports.vacationListData = [
+    {
+        "Id": 295,
+        "userName": "주영성",
+        "typeDetail": "연차",
+        "start": "12/20",
+        "end": "12/21",
+        "status": "submit"
+    },
+    {
+        "Id": 294,
+        "userName": "주영성",
+        "typeDetail": "연차",
+        "start": "12/20",
+        "end": "12/21",
+        "status": "submit"
+    },
+    {
+        "Id": 293,
+        "userName": "주영성",
+        "typeDetail": "연차",
+        "start": "12/20",
+        "end": "12/21",
+        "status": "accept"
+    }
+]
+
+exports.vacationDetailData ={
+    "Id": 293,
+    "userName": "주영성",
+    "typeDetail": "연차",
+    "start": "12/20",
+    "end": "12/21",
+    "status": "accept"
+}
